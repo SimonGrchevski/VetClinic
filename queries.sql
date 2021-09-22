6cf5db73f841e37a7aa0feeeab13bfafad485b76
SimonGrchevski <simongrcevski@gmail.com>
2021-09-22 20:40:03 +0200

    Fix the order of the statements

--- queries.sql
+++ queries.sql
@@ -21,19 +21,34 @@ SELECT* FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
                   -- Milestone 2
 -- How many animals are there?
-SELECT count(*) FROM animals;
+SELECT COUNT(*) 
+FROM animals;
 -- How many animals have never tried to escape?
-SELECT count(*) FROM animals WHERE escape_attempts = 0 GROUP BY escape_attempts;
+SELECT COUNT(*) 
+FROM animals 
+WHERE escape_attempts = 0 GROUP BY escape_attempts;
 -- Check the names and escape_attempts if the query was correct( extra )
-SELECT name, escape_attempts FROM animals WHERE escape_attempts= 0 GROUP BY name,escape_attempts;
+SELECT name, escape_attempts 
+FROM animals 
+WHERE escape_attempts= 0 
+GROUP BY name,escape_attempts;
 -- What is the average weight of animals?
-SELECT AVG(weight_kg) AS "Average Weight" from animals;
+SELECT AVG(weight_kg) AS "Average Weight" 
+FROM animals;
 -- Who escapes the most, neutered or not neutered animals?
-select neutered, avg(escape_attempts) as "Avarage Escape Attempts" from animals group by neutered;
+SELECT neutered, AVG(escape_attempts) AS "Avarage Escape Attempts" 
+FROM animals 
+GROUP BY neutered;
 -- What is the minimum and maximum weight of each type of animal?
-SELECT min(weight_kg), max(weight_kg) from animals;
+SELECT MIN(weight_kg), MAX(weight_kg) 
+FROM animals;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT avg(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+SELECT species, AVG(escape_attempts) 
+FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31'
+GROUP BY species; 
+
+
 
 
 

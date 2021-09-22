62c119904afe54f379d9576263fe402577c96f06
SimonGrchevski <simongrcevski@gmail.com>
2021-09-22 18:22:36 +0200

    Write all the queries for the milestone2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-
+                  -- Milestone 1
 -- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
 -- List the name of all animals born between 2016 and 2019.
@@ -17,3 +17,20 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT* FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+                  -- Milestone 2
+-- How many animals are there?
+SELECT count(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT count(*) FROM animals WHERE escape_attempts = 0 GROUP BY escape_attempts;
+-- Check the names and escape_attempts if the query was correct( extra )
+SELECT name, escape_attempts FROM animals WHERE escape_attempts= 0 GROUP BY name,escape_attempts;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) AS "Average Weight" from animals;
+-- Who escapes the most, neutered or not neutered animals?
+select neutered, avg(escape_attempts) as "Avarage Escape Attempts" from animals group by neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT min(weight_kg), max(weight_kg) from animals;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT avg(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';

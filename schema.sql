4eef7d9578e3a1a7376bec0c566340d874ce6358
SimonGrchevski <simongrcevski@gmail.com>
2021-09-24 16:18:42 +0200

    Add new tables and add columns to animals table and make them foreign keys

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
 DROP TABLE IF EXISTS animals;
-
 CREATE TABLE animals (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name char(50),
@@ -11,5 +10,41 @@ CREATE TABLE animals (
     weight_kg decimal,
 );
 
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners (
+    id int GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(32),
+    age int,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS species;
+CREATE TABLE species (
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(32),
+    PRIMARY KEY(id)
+);
+
 ALTER TABLE animals
 ADD species char(50);
+
+ALTER TABLE animals
+DROP species;
+
+ALTER TABLE animals
+ADD species_id int;
+
+ALTER TABLE animals
+ADD owner_id int;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_fk
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_fk
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
